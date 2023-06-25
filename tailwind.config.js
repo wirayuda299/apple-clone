3e3fa75f89b909a94656d1ad0e461512e2550a9d
wirayuda <wirayuda233@gmail.com>
2023-06-24 23:02:52 -0700

    remove unused code

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,43 +13,8 @@ module.exports = {
           '100%': { opacity: '100' }
         }
       },
-      slide: {
-        "0%": {
-          height: "0px",
-          top: "-100%",
-          left: "0px",
-          height: "0px",
-          display: "none"
-        },
-        "100%": {
-          height: "100%",
-          top: "0px",
-          left: "0px",
-          height: "100vh",
-          display: "block"
-        }
-      },
-      slideOut: {
-        "0%": {
-          height: "100%",
-          top: "0",
-          left: "0",
-          display: "block",
-          height: "100vh"
-        },
-        "100%": {
-          height: "0",
-          top: "-100%",
-          left: "0",
-          height: "0",
-          display: "none"
-        },
-
-      },
       animation: {
         "fadeIn": 'fadein 1s ease-in-out forwards',
-        "slide": "slide 500ms ease-in forwards",
-        "slideOut": "slideOut 500ms ease-in forwards"
       }
     },
   },
